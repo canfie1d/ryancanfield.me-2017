644e439db03f9936f01fe0a5320e9be73a05cb65
Ryan <ryancanfield@me.com>
2017-03-01 19:17:07 -0800

    remove redux logger from production

--- src/Services/Store.js
+++ src/Services/Store.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { browserHistory } from 'react-router';
 import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
@@ -6,14 +7,17 @@ import thunk from 'redux-thunk';
 import reducers from '../Reducers';
 import { responsiveStoreEnhancer } from 'redux-responsive';
 
+let middleware = [ thunk, routerMiddleware(browserHistory) ];
+
+if (process.env.NODE_ENV !== 'production') {
+  let logger = createLogger();
+  middleware = [ ...middleware, logger ];
+}
+
 export default function () {
   return createStore(
     reducers,
     responsiveStoreEnhancer,
-    applyMiddleware(
-      thunk,
-      createLogger(),
-      routerMiddleware(browserHistory)
-    )
+    applyMiddleware(...middleware)
   );
 }
